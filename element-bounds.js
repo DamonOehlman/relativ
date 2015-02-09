b455adb37b4f9d4ce1aaa7aeba97767b0904333c
Damon Oehlman <damon.oehlman@gmail.com>
2015-02-09 17:29:13 +1100

    expose element-bounds calculation

---
+++ element-bounds.js
@@ -0,0 +1,23 @@
+module.exports = function(el) {
+  var rect = el && el.getBoundingClientRect();
+  var targetX = 0;
+  var targetY = 0;
+  var targetWidth = 0;
+  var targetHeight = 0;
+
+  // get the width and height from the client bounding rect
+  targetWidth = rect ? rect.width : 0;
+  targetHeight = rect ? rect.height : 0;
+
+  // get the offset from offsetLeft and offsetTop
+  targetX = 0;
+  targetY = 0;
+  while (el) {
+    targetX += el.offsetLeft;
+    targetY += el.offsetTop;
+
+    el = el.offsetParent;
+  }
+
+  return [targetX, targetY, targetWidth, targetHeight];
+};
--- element.js
+++ element.js
@@ -1,23 +1,3 @@
 module.exports = function(el) {
-  var rect = el && el.getBoundingClientRect();
-  var targetX = 0;
-  var targetY = 0;
-  var targetWidth = 0;
-  var targetHeight = 0;
-
-  // get the width and height from the client bounding rect
-  targetWidth = rect ? rect.width : 0;
-  targetHeight = rect ? rect.height : 0;
-
-  // get the offset from offsetLeft and offsetTop
-  targetX = 0;
-  targetY = 0;
-  while (el) {
-    targetX += el.offsetLeft;
-    targetY += el.offsetTop;
-
-    el = el.offsetParent;
-  }
-
-  return require('./')([targetX, targetY, targetWidth, targetHeight]);
+  return require('./')(require('./element-bounds')(el));
 };
