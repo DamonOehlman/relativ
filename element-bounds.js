6f0b886113b0416119571808801ba9c56d1294b3
Nathan Oehlman <nathan.oehlman@gmail.com>
2015-08-26 16:32:25 +1000

    Add two optional values, scaleX and scaleY to the returned relativ array

--- element-bounds.js
+++ element-bounds.js
@@ -4,20 +4,20 @@ module.exports = function(el) {
   var targetY = 0;
   var targetWidth = 0;
   var targetHeight = 0;
+  var scaleX = 1;
+  var scaleY = 1;
 
   // get the width and height from the client bounding rect
   targetWidth = rect ? rect.width : 0;
   targetHeight = rect ? rect.height : 0;
 
-  // get the offset from offsetLeft and offsetTop
-  targetX = 0;
-  targetY = 0;
-  while (el) {
-    targetX += el.offsetLeft;
-    targetY += el.offsetTop;
+  // Use the bounding rects left & top
+  targetX = rect ? rect.left : 0;
+  targetY = rect ? rect.top : 0;
 
-    el = el.offsetParent;
-  }
+  // Calculate the scale factors
+  scaleX = el ? targetWidth / el.offsetWidth : 0;
+  scaleY = el ? targetHeight / el.offsetHeight : 0;
 
-  return [targetX, targetY, targetWidth, targetHeight];
+  return [targetX, targetY, targetWidth, targetHeight, scaleX, scaleY];
 };
